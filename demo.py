8ae8e2d6e2d2f44c3105fe60f754385755ac06c2
farkas <zoltan.farkas1028@gmail.com>
2021-11-26 16:01:20 +0100

    demo.py es nevek.txt hozzaadasa

---
+++ demo.py
@@ -0,0 +1,9 @@
+# Python program a páros/páratlan számok szétválasztásá
+# Egy szám páros, ha kettővel osztva a maradék 0
+# Ha a maradék 1, a szám páratlan
+
+num = int(input("Adj meg egy számot: "))
+if (num % 2) == 0:
+   print("{0} párs".format(num))
+else:
+   print("{0} páratlan".format(num))
